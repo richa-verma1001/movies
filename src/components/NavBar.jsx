901a645d80fd166fb86b57a7794600ef9461d20e
Richa Verma <richa.verma1001@gmail.com>
2022-02-25 14:17:45 +0500

    Partial: Login Component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,6 +8,7 @@ export default function NavBar() {
         <li><NavLink to="/movies">Movies</NavLink></li>
         <li><NavLink to="/customers">Customers</NavLink></li>
         <li><NavLink to="/rentals">Rentals</NavLink></li>
+        <li><NavLink to="/login">Login</NavLink></li>
       </ul>
     </div>
   )
---
+++ src/components/common/Input.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function Input(props) {
+  const {
+    label,
+    name,
+    value,
+    type,
+    onChange,
+    error } = props;
+
+  const showError = error != '' ? true : false;
+
+  return (
+    <>
+      <label htmlFor={name}>{label}</label>
+      <input
+        autoFocus
+        name={name}
+        value={value}
+        id={name}
+        type={type}
+        onChange={onChange} />
+      <span className={showError ? 'error' : ''}>{error}</span>
+    </>
+  )
+}
