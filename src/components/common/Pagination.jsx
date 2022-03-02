37d1554a3314de201991d0ad22772af931114a3a
Richa Verma <richa.verma1001@gmail.com>
2022-03-02 08:03:09 +0500

    fix: unused imports

--- src/components/common/Pagination.jsx
+++ src/components/common/Pagination.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { getMoviesPerPage, getPageCount } from '../../utils/paginationHelper';
-
 
 export default class Pagination extends Component {
   render() {
